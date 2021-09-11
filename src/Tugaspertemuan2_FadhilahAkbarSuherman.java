cc43d555454401f98cc81ffb128e041316472217
fadhilahakbars <fadhilah.akbar@ui.ac.id>
2021-09-11 08:24:59 +0700

    first commit

---
+++ src/Tugaspertemuan2_FadhilahAkbarSuherman.java
@@ -0,0 +1,24 @@
+public class Tugaspertemuan2_FadhilahAkbarSuherman {
+        public static void main (String[] args){
+
+            System.out.println("Hello selamat datang mahasiswa OOP");
+
+            int myfirstnumber = (5 + 10) + (2 * 10);
+            int mySecondnumber = 12;
+            int myThirdnumber = myfirstnumber * 6;
+            int myTotal = myfirstnumber + mySecondnumber + myThirdnumber;
+            int myLastOne = 1000 - myTotal;
+            System.out.println(myLastOne);
+
+            int angka1 = 7;
+            int angka2 = 3;
+            int x;
+            int y = 3;
+
+            for (x = 0; x<=y+1; x++ ) {
+                int hasil = (x>y) ? (5 + 3) : (6 - 2);
+                System.out.println(hasil);
+            }
+        }
+}
+
